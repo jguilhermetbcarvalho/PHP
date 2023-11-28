68e36cfab33a9a482b8ad8a960bbbe2130a8f0b6
jguilhermetbcarvalho <joaogcarvalho012@gmail.com>
2023-11-28 11:31:12 -0400

    EX 00 AO EX03

---
+++ ex003/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Tipos primitivos em PHP</title>
+</head>
+<body>
+    <h1>Teste de tipos primitivos</h1>
+    <?php 
+        // 0x = Hexadecimal
+        // 0b = Binário
+        // 0 = Octal
+
+        // $numInt = 300;
+        // $numHex = 0x1A;
+        // $numBin = 0b1011;
+        // $numOct = 010;
+        // echo "Os valores das variáveis são $numInt, $numHex, $numBin, $numOct"
+
+        // $valor = 123;
+        // var_dump($valor)
+
+        // $num = 3e2; // 3 x 10(2)
+        // echo "O valor é $num";
+        // var_dump($num) // Irá retornar float, pois valores com potência são atribuidos como float
+
+        // $numInteger = (int) 3e2; // Coerção
+        // var_dump($num) // Irá retornar interger
+
+        // $casadi = true;
+        // var_dump($casado);
+        // echo "O valor para casado é $casado"; // Quando for true, irá aparecer 1, e se for false, não irá aparecer nada
+
+        // $vet = [6, 2.5, "Maria", 3, false];
+        // var_dump($vet);
+
+        // class Pessoa {
+            // private string $nome;
+        // }
+        // $p = new Pessoa;
+        // var_dump($p)
+    ?>
+</body>
+</html>
